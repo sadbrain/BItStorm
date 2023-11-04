6474c7ac4b9864c195e39175ceacba75d1920918
sadbrain <riotgheptrannhulol@gmail.com>
2023-11-04 16:59:20 -0700

    Add project files.

---
+++ BitStorm/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BitStorm.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        //connecting with entity framework
+        //configure DB context
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ BitStorm/Migrations/20231104235552_create_database_BitStorm.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BitStorm.Migrations
+{
+    public partial class create_database_BitStorm : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
