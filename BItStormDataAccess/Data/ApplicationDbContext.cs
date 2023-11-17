7388393bc3fa41bf960d182c73f855a430645255
sadbrain <riotgheptrannhulol@gmail.com>
2023-11-17 04:56:10 -0800

    done post

--- BItStormDataAccess/Data/ApplicationDbContext.cs
+++ BItStormDataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ public class ApplicationDbContext : DbContext
     {
 
     }
+ 
     public DbSet<User> Users { get; set; }
     public DbSet<Expert> Experts { get; set; }
     public DbSet<Role> Roles { get; set; }
